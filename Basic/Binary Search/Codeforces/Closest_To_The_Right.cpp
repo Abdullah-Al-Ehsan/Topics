8943810e6e807a7011ce470122e5a91e3725108c
Abdullah-Al-Ehsan <alehsan019@gmail.com>
2025-04-19 02:49:49 +0600

    Create Closest_To_The_Right.cpp

---
+++ Basic/Binary Search/Codeforces/Closest_To_The_Right.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define ll long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define ceill(a,b) ((a+b-1)/b)                                                                                                                                                                   
+ll power(ll b,ll i) { ll result = 1; while (i > 0)   {  if (i % 2 == 1)  result *= b; b *= b; i /= 2; } return result;}
+bool isPrime (ll n ) { if ( n <= 1 ) return false;ll sqrtn = sqrt(n); for (ll i = 2; i <= sqrtn; i++ ) { if ( n % i == 0 ) return false;} return true;}
+const int Mod=1e9+7;
+const ll MAX=1e15;
+int Num=1000000+7;
+
+    void solve()
+    {
+          ll n,k;
+          cin>>n>>k;
+          vector<ll>a(n),q(k);
+          for(int i=0;i<n;i++)cin>>a[i];
+          for(int i=0;i<k;i++)cin>>q[i];
+          sort(a.begin(),a.end());
+          
+          for(int i=0;i<k;i++)
+          {
+                  int low=0;
+                  int high=n;
+                  ll mid;
+                  
+                  while(low<high)
+                  {      
+                        mid=(low+high)/2;
+                        if(a[mid]>=q[i])
+                        {
+                            high=mid;
+                        }
+                        else 
+                        {
+                             low=mid+1;
+                        }
+                  }
+                  cout<<low+1<<endl;
+          }
+    }
+int main()
+{    
+    fast_io;
+    solve();
+}
